048c6d98cb2dc62586e1d0dd918d21aae48b5652
tpham20908 <tpham20908@gmail.com>
2018-10-01 22:00:30 -0400

    created title bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import book from './book.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi there</h1>
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img alt="book" src={book} />
+              </td>
+              <td>
+                <h1>Google Books Search</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
